492d5c2fd5dbf1412db493efa2bc22d96fe9b01e
Glitch (ttay921-spotifyglitch) <none>
2019-02-13 14:21:41 +0000

    📷🚕 Checkpoint ./views/index.html:3109015/252 ./server.js:3109015/315 ./public/client.js:3109015/60

--- server.js
+++ server.js
@@ -109,20 +109,6 @@ app.get('/audio-features', function (request, response) {
     });
   
   
-  // Get the audio features for a track ID
-  spotifyApi.getAudioFeaturesForTrack('4uLU6hMCjMI75M1A2tKUQC')
-    .then(function(data) {
-    
-      //Send the audio features object
-      response.send(data.body);
-    
-
-    
-    }, function(err) {
-      console.error(err);
-    });
-  
-   
   
 });
   
